64d4cbdbb030e678257d98682a0d9e8b0fcd3146
Edwin K Sunny <127194197+edweeh@users.noreply.github.com>
2023-12-17 22:48:55 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from './components/Navbar';
+import Login from './components/Loginpage/Login';
+
+// import LoginPage from './LoginPage'; // Adjust the path based on your project structure
+
+const App = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+};
+
+export default App;
+
