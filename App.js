5caae954ceec201b57c3431c71bf217277b4e101
Edwin K Sunny <127194197+edweeh@users.noreply.github.com>
2023-12-20 11:26:06 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
 
-import React from 'react';
-import AdminPage from './components/Adminside/Adminpage';
-import Login from './components/Login/Login';
-
-import Psignup from './components/Psignup';
-
-// import Navbar from './components/Navbar';
-// import LoginPage from './LoginPage'; // Adjust the path based on your project structure
-
-const App = () => {
+function App() {
   return (
-    <div>
-      {/* <Navbar/> */}
-      {/* <Psignup/> */}
-      {/* <Login/> */}
-      <AdminPage/>
-      
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
-
